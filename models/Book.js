14d3129e561aeae9260068ef54c39ed7ceb750b5
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-16 14:42:13 -0600

    Set up Book model

--- models/Book.js
+++ models/Book.js
@@ -3,7 +3,21 @@ const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
-    
+    title: {
+        type: String
+    },
+    authors: [{
+        type: String
+    }],
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    link: {
+        type: String
+    }
 });
 
 var Book = mongoose.Model("Book", BookSchema);

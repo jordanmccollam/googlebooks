61c8b0d60ed55a1358f7bba1425e123232395f85
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-16 18:33:06 -0600

    Changed all the Builds to Public directory and added API key

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 
 // Connect to Mongo
@@ -22,7 +22,7 @@ require("./routes/apiRoutes")(app);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {

d7a8968720c1d95314a12244fb22dbe7341960b9
Jordan McCollam <jordy.mccollam@gmail.com>
2019-12-17 13:50:12 -0600

    Restyled h1

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -16,12 +16,12 @@ function Header() {
                 <span className="text-danger">o</span>
                 <span className="text-warning">o</span>
                 <span className="text-primary">k </span>
-                <span className="text-success">S</span>
+                <span className="text-primary">S</span>
                 <span className="text-danger">e</span>
-                <span className="text-primary">a</span>
-                <span className="text-danger">r</span>
-                <span className="text-warning">c</span>
-                <span className="text-primary">h</span>
+                <span className="text-warning">a</span>
+                <span className="text-primary">r</span>
+                <span className="text-success">c</span>
+                <span className="text-danger">h</span>
             </h1>
             <button className="btn btn-primary px-5 mr-2">Search</button>
             <button className="btn btn-outline-primary px-5">Saved</button>
